fa51e052d08a5f7b01d15426c42de432a219fead
Leandro Santos <contato@oleandrosantos.me>
2022-02-25 15:59:02 -0300

    feat: criando functions da classe calculadora

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,28 +1,122 @@
+using System;
+using System.Collections;
+using System.Globalization;
+
 namespace Classes
 {
-
-
-    class Calculadora{
-
-        public static int somarNumeros(int numero1 = 0, int numero2 = 0){
-            if(numero1 == null || numero2 == null){
-                return 0;
-            }else{
-                return (numero1 + numero2);
-            }
-        }
-
-        public static int subtrairNumeros(int numero1, int numero2){
-            return (numero1 + numero2);
-        }
-
-        public static double dividirNumeros(int numero1, int numero2){
-            return((double)numero1/(double)numero2);
-        }
-
-        public static int multiplicarNumeros(int numero1, int numero2){
-            return numero1*numero2;
-        }
-
+  class Calculadora
+  {
+    public static double somarNumeros(List<double> numero)
+    {
+      return (numero[0] + numero[1]);
     }
+
+    public static double subtrairNumeros(List<double> numero)
+    {
+      return (numero[0] - numero[1]);
+    }
+
+    public static double dividirNumeros(List<double> numero)
+    {
+      return (numero[0] / numero[1]);
+    }
+
+    public static double multiplicarNumeros(List<double> numero)
+    {
+      return numero[0] * numero[1];
+    }
+
+    public static void menuCalculadora()
+    {
+      bool repetirMenu = true;
+      ushort? comandos = null;
+
+      while (repetirMenu)
+      {
+        Console.Clear();
+        Console.WriteLine("Bem vindo a calculadora!!");
+        Console.WriteLine("Abaixo segue a lista de comandos, digite conforme o numero");
+        Console.WriteLine("1 => Somar");
+        Console.WriteLine("2 => Subtrair");
+        Console.WriteLine("3 => Dividir");
+        Console.WriteLine("4 => Multiplicar");
+        Console.WriteLine("0 => Sair");
+        Console.Write("O que deseja? ");
+
+        var EntradaDeDados = Console.ReadLine();
+        try
+        {
+          comandos = ushort.Parse(EntradaDeDados);
+          if (comandos > 4)
+          {
+            throw new Exception("comando invalido, porfavor, digite um comando valido!");
+          }
+          CalcularEExibir(comandos);
+          comandos = 0;
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e);
+        }
+
+      }
+    }
+
+    public static void CalcularEExibir(ushort? comando)
+    {
+      double resultado = 0;
+      if (comando < 1)
+      {
+        Console.WriteLine("Obrigado! aperte Enter para encerrar!");
+        Console.ReadLine();
+        System.Environment.Exit(10);
+      }
+      List<double> numeros = capturarNumeros();
+      switch (comando)
+      {
+        case 1: resultado = somarNumeros(numeros); break;
+        case 2: resultado = subtrairNumeros(numeros); break;
+        case 3: resultado = dividirNumeros(numeros); break;
+        case 4: resultado = multiplicarNumeros(numeros); break;
+      }
+
+      string specifier = "G";
+      Console.WriteLine($"O resultado da operacao é " + resultado.ToString(specifier, CultureInfo.InvariantCulture));
+      Console.ReadLine();
+    }
+
+    public static List<double> capturarNumeros()
+    {
+      List<double> listaDeNumeros = new List<double>();
+      double numero;
+      bool temosDoisNumeros = false;
+
+      while (!temosDoisNumeros)
+      {
+        string position = "primeiro";
+
+        if (listaDeNumeros.Count > 0)
+          position = "segundo";
+
+
+        try
+        {
+          Console.Write($"Digite o {position} numero da equação: ");
+          numero = double.Parse(Console.ReadLine());
+          listaDeNumeros.Add(numero);
+        }
+        catch
+        {
+          Console.WriteLine("O Numero digitado é invalido, digite novamente outro numero!");
+          Thread.Sleep(300);
+        }
+
+        if (listaDeNumeros.Count > 1)
+          temosDoisNumeros = true;
+
+      }
+
+      return listaDeNumeros;
+    }
+  }
 }
