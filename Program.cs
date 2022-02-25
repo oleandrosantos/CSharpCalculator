c0021932ba9f3490797f1a89f00e34473e3f3b1b
Leandro Santos <contato@oleandrosantos.me>
2022-02-25 15:59:29 -0300

    refactor: refatorando o class principal

--- Program.cs
+++ Program.cs
@@ -3,38 +3,8 @@
 namespace CalculadoraPrograma;
 class Program
 {
-    static void Main(string[] args)
-    {
-        int numero1 = 0;
-        int numero2 = 0;
-
-
-        while(numero1 == 0 || numero2 == 0){
-            string? valorDigitado;
-            
-            if(numero1 == 0){
-                Console.Write("Digite um numero: ");
-                valorDigitado = Console.ReadLine();
-                try{
-                    numero1 = Int32.Parse(valorDigitado);
-                    valorDigitado = null;
-                }catch{
-                    Console.WriteLine("Digite um numero inteiro!");
-                }
-            }else{
-                Console.Write("Digite outro numero: ");
-                valorDigitado = Console.ReadLine();
-                try{
-                    numero2 = Int32.Parse(valorDigitado);
-                    valorDigitado = null;
-                }catch{
-                    Console.WriteLine("Digite um numero inteiro!");
-                }
-            }            
-
-
-        }
-
-        Console.WriteLine(Calculadora.somarNumeros(numero1, numero2));
-    }
+  static void Main(string[] args)
+  {
+    Calculadora.menuCalculadora();
+  }
 }
